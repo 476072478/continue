aa92640774dd24f915489d98ccefe6caf7fe415d
小唐 <476072478@qq.com>
2022-11-22 20:38:07 +0800

    修改

--- js深度学习/call_apply_bind.js
+++ js深度学习/call_apply_bind.js
@@ -13,10 +13,13 @@ Function.prototype.myapply = function (context, args) {
     context.fn(...args)
     delete context.fn
 }
-Function.prototype.mybind = function (context, args) {
-    context.fn = this
-    context.fn(...args)
-    delete context.fn
+Function.prototype.mybind = function (context, ...args) {
+    let fn = this
+    let newcontext = JSON.parse(JSON.stringify(context))
+    newcontext.fn = fn
+    return function (...args) {
+        return newcontext.fn(...args)
+    }
 }
 function callmyname(a, b, c) {
     console.log(this.name)
@@ -25,4 +28,6 @@ function callmyname(a, b, c) {
 // callmyname('a', 'b', 'c')
 // callmyname.mycall(obj, 'a', 'b', 'c')
 // callmyname.apply(obj, ['a', 'b', 'c'])
-callmyname.bind(obj, 'a', 'b', 'c')
+// console.log(callmyname.bind(obj, 'a', 'b', 'c'))
+// console.log(callmyname.mybind(obj, 'a', 'b', 'c'))
+callmyname.bind(obj, '1', '2', '3')()
